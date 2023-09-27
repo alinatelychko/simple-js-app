0dde4b9450eebae39cf53a1d10a6cdadb862fbf8
alinatelychko <adelin9999@gmail.com>
2023-09-27 14:37:11 +0200

    3rd commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-alert('Hello world');
+let pokemonList = [{name: 'Bulbasar', height: 0.7, type: ['Monster','grass']},
+{name: 'Charmeleon', height: 1.1, type: ['Monster','Dragon']},
+{name: 'Squirtle', height: 0.5, type: ['water']}
+{name: 'Metapod', height: 0.7, type: ['bug']}
+{name: 'Rattata', height: 0.3, type: ['field']}
+];
 
-let favoriteFood = 'sushi';
-document.write(favoriteFood);
+
