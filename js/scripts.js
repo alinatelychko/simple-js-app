6f8ddf07aab082d0dee2cc349e8cd233b52356dc
alinatelychko <adelin9999@gmail.com>
2023-09-29 15:16:57 +0200

    <task 1.3>

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,25 @@
-let pokemonList = [{name: 'Bulbasar', height: 0.7, type: ['Monster','grass']},
-{name: 'Charmeleon', height: 1.1, type: ['Monster','Dragon']},
-{name: 'Squirtle', height: 0.5, type: ['water']}
-{name: 'Metapod', height: 0.7, type: ['bug']}
-{name: 'Rattata', height: 0.3, type: ['field']}
+let pokemonList = [
+{name: 'Bulbasar', 
+ height: 0.7, 
+ type: ['Monster','grass']},
+{name: 'Charmeleon', 
+ height: 1.1,
+ type: ['Monster','Dragon']},
+{name: 'Squirtle',
+ height: 0.5, 
+type: ['water']},
+{name: 'Metapod', 
+height: 0.7, 
+type: ['bug']},
+{name: 'Rattata', 
+height: 0.3, 
+type: ['field']}
 ];
 
 
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height > 1)
+   {document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "- Wow. That\'s big!"+ "<br>"); //condition for pokemon with the height > 1 
+} else {
+document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
+}
