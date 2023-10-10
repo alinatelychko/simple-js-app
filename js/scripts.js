72671e1577b01cf2b89dba8c2e54ab89748f41db
alinatelychko <adelin9999@gmail.com>
2023-10-10 13:28:47 +0200

    <task 1.5 forEach>

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,33 @@
 let pokemonList = [
-{name: 'Bulbasar', 
- height: 0.7, 
- type: ['Monster','grass']},
-{name: 'Charmeleon', 
- height: 1.1,
- type: ['Monster','Dragon']},
-{name: 'Squirtle',
- height: 0.5, 
-type: ['water']},
-{name: 'Metapod', 
-height: 0.7, 
-type: ['bug']},
-{name: 'Rattata', 
-height: 0.3, 
-type: ['field']}
+  { name: "Bulbasar", height: 0.7, type: ["Monster", "grass"] },
+  { name: "Charmeleon", height: 1.1, type: ["Monster", "Dragon"] },
+  { name: "Squirtle", height: 0.5, type: ["water"] },
+  { name: "Metapod", height: 0.7, type: ["bug"] },
+  { name: "Rattata", height: 0.3, type: ["field"] },
 ];
 
 
-for (let i = 0; i < pokemonList.length; i++) {
-  if (pokemonList[i].height > 1)
-   {document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "- Wow. That\'s big!"+ "<br>"); //condition for pokemon with the height > 1 
-} else {
-document.write(pokemonList[i].name + "  " + "(height:"+ "  " +  pokemonList[i].height + " ) " + "<br>");}
-}
+pokemonList.forEach(function(pokemon){
+  if (pokemon.height > 1) {
+        document.write(
+          pokemon.name +
+            "  " +
+            "(height:" +
+            "  " +
+            pokemon.height +
+            " ) " +
+            "- Wow. That's big!" +
+            "<br>"
+        ); //condition for pokemon with the height > 1
+      } else {
+        document.write(
+          pokemon.name +
+            "  " +
+            "(height:" +
+            "  " +
+            pokemon.height +
+            " ) " +
+            "<br>"
+        );
+      }
+    });
