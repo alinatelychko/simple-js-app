f1c334b0f850c2d6a4abdd0577e958e8ec1eb278
alinatelychko <adelin9999@gmail.com>
2023-10-18 09:37:31 +0200

    task 1.7

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,79 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { name: "Bulbasar", height: 0.7, type: ["Monster", "grass"] },
-    { name: "Charmeleon", height: 1.1, type: ["Monster", "Dragon"] },
-    { name: "Squirtle", height: 0.5, type: ["water"] },
-    { name: "Metapod", height: 0.7, type: ["bug"] },
-    { name: "Rattata", height: 0.3, type: ["field"] },
-  ];
+  let pokemonList = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150";
 
- function showDetails(pokemon) { 
-  console.log(pokemon.target.innerText); 
-}
-  
+
   function addListItem(pokemon) {
     let element = document.querySelector(".pokemon-list");
     let listItem = document.createElement("li");
     let button = document.createElement("button");
     button.innerText = pokemon.name;
+    button.classList.add("button-class");
     listItem.appendChild(button);
     element.appendChild(listItem);
-    button.addEventListener('click', showDetails);
-  };
+    button.addEventListener("click", showDetails(pokemon));
+  }
+
+  function loadList() {
+    return fetch(apiUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (json) {
+        json.results.forEach(function (item) {
+          let pokemon = {
+            name: item.name,
+            detailsUrl: item.url,
+          };
+          add(pokemon);
+        });
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (details) {
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      })
+      .catch(function (e) {
+        console.error(e);
+      });
+  }
+ 
+  function  add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
+  }
 
   return {
     getAll: function () {
       return pokemonList;
-    },
-    add: function (item) {
-      pokemonList.push(item);
-    },
-    addListItem: addListItem
+    }, 
+    add,
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
-console.log(pokemonRepository.getAll());
-console.log(
-  pokemonRepository.add({ name: "Rattata 2", height: 0.3, type: ["field"] })
-);
+// console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach(function (pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function () {
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
